c158b4985f9c20680869b43df8b5343c999e2d68
Ritesh Gupta <104141297+RitesH000027@users.noreply.github.com>
2024-12-05 01:03:13 +0530

    Update wsgi.py

--- api/src/wsgi.py
+++ api/src/wsgi.py
@@ -1,5 +1,10 @@
+import sys
+import os
 from api.src.main import app
 
+# Add the current directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 if __name__ == "__main__":
+    # Run the application
     app.run()
